6efd6f94e21213eb1ad91571c6f803b266db34b3
Aslan <korpeevaslan@gmail.com>
2021-01-24 23:47:21 +0500

    start develop react tod app || creating ui and styling

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import listSvg from "./assets/img/list.svg"
+
+function App() {
+  return (
+    <div className='todo'>
+      <div className='todo__sidebar'>
+        <ul className='todo__list'>
+          <li></li>
+        </ul>
+      </div>
+      <div className='todo__tasks'></div>
+    </div>
+  )
+}
+
+export default App
